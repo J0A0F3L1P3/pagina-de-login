d1d4dda9856eb7873a89c713d84326d80e7584cc
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2023-04-07 19:35:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Este site possui uma tecnologia que disponibiliza uma imagem de perfil aleatória sempre que a página é atualizada. \n\nAliás JavaScript iniciado")
+//alert("Este site possui uma tecnologia que disponibiliza uma imagem de perfil aleatória sempre que a página é atualizada. \n\nAliás JavaScript iniciado")
